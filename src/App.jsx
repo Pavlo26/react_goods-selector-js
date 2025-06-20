02cdd360279ce1d89cb486c301c72b9f551405b3
Svystun Pavlo <spo26081985@gmail.com>
2025-06-20 22:17:20 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import 'bulma/css/bulma.css';
 import './App.scss';
+import { useState } from 'react';
 
 export const goods = [
   'Dumplings',
@@ -14,16 +15,41 @@ export const goods = [
   'Garlic',
 ];
 
-export const App = () => (
-  <main className="section container">
-    <h1 className="title is-flex is-align-items-center">No goods selected</h1>
+export const App = () => {
+  const [value, setValue] = useState('');
 
+  return (
+  <main className = "section container" >
     <h1 className="title is-flex is-align-items-center">
-      Jam is selected
-      <button data-cy="ClearButton" type="button" className="delete ml-3" />
+      {value ? `${value} is selected` : 'No goods selected'}
+      {value && <button data-cy="ClearButton" type="button" className="delete ml-3" onClick={() => setValue('')}/>}
     </h1>
 
     <table className="table">
+      {goods.map(good => (
+        <tbody>
+          <tr key={good} data-cy="Good" className={good === value ? 'has-background-success-light' : ''}>
+            <td>
+              <button
+              data-cy={good === value ? 'AddButton' : 'RemoveButton'}
+              type="button"
+              className={good === value ? 'button is-info' : 'button'}
+                onClick={() => {
+                  if (good === value) {
+                    setValue('');
+                  } else {
+                    setValue(good);
+                  }}}>
+                {good === value ? '-' : '+'}
+              </button>
+            </td>
+
+            <td data-cy="GoodTitle" className="is-vcentered">
+              {good}
+            </td>
+          </tr>
+        </tbody>
+      ))}
       <tbody>
         <tr data-cy="Good">
           <td>
@@ -67,4 +93,4 @@ export const App = () => (
       </tbody>
     </table>
   </main>
-);
+)};
